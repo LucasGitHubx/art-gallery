cb254330a7ba05b63e03a1a3ea98b55a4e4bd1f6
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-03 18:16:07 -0300

    Adding the photo component to the Main Page.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,26 @@
 import { useState, useEffect } from "react";
+import { getAllPhotos } from "../firebase/firestore";
+import Photo from "../components/Photo";
 
 export default function MainPage() {
+  const [data, setData] = useState([]);
+
   const [tag, setTag] = useState("");
   const [loaded, setLoaded] = useState(false);
+  const [label, setLabel] = useState("Search by tag");
+
+  useEffect(() => {
+    getAllPhotos(setData, setLoaded);
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <div className="main-page page">
-      <form>
-        <label>Search by tag</label>
+      <form onSubmit={handleSubmit}>
+        <label>{label}</label>
         <div>
           <input
             type="text"
@@ -18,6 +31,18 @@ export default function MainPage() {
           <button>Submit</button>
         </div>
       </form>
+
+      {!loaded ? (
+        <h2>Loading...</h2>
+      ) : data.length == 0 ? (
+        <h2>There are no photos for the moment...</h2>
+      ) : (
+        <div className="photos">
+          {data.map((photo) => {
+            return <Photo photo={photo} key={photo.id} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
