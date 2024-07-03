23f55cc376c6e4a8b96fe076a67a9d16e420bbef
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-03 18:14:50 -0300

    I added the image, the title and author of the image. Besides all that, I coded a button that can delete the photo from the database.

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,3 +1,24 @@
 export default function Photo({ photo }) {
-  return <div className="photo"></div>;
+  return (
+    <div className="photo">
+      <img src={photo.url} alt={photo.title} />
+      <div className="information">
+        <div className="main-information">
+          <h3 className="title">{photo.title}</h3>
+          <p>
+            by <i>{photo.author}</i>
+          </p>
+        </div>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
 }
+
+/* 
+NEXT THINGS TO GET DONE:
+
+1) Add functionality to the delete button.
+2) Develop the other pages (About us, add a photo and my favourites.)
+3) Make the tag searcher work.
+*/
