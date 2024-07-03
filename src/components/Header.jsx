226781db815daacaa977f15a1f458b695a3f72b8
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-03 15:14:21 -0300

    Creating the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <h1>
+        <NavLink>Art Gallery</NavLink>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink>About Us</NavLink>
+          </li>
+          <li>
+            <NavLink>Add a photo</NavLink>
+          </li>
+          <li>
+            <NavLink>My favourites</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
