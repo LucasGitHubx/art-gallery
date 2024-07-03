b97f4d3ee704d4bbdeecbaa906ca596755d42784
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-03 17:50:10 -0300

    I created a function to fetch the data from the "photos" collection.

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,8 +1,25 @@
 import { app } from "./firebase";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, query, getDocs, collection } from "firebase/firestore";
 
 const db = getFirestore(app);
+const photoRef = collection(db, "photos");
 
-export function data() {
-  console.log(db);
+export async function getAllPhotos(setData, setLoaded) {
+  const photos = [];
+  const q = query(photoRef);
+  const data = await getDocs(q);
+
+  data.docs.forEach((doc) => {
+    photos.push({
+      id: doc.id,
+      title: doc.data().title,
+      author: doc.data().author,
+      url: doc.data().url,
+      tags: doc.data().tags,
+    });
+    setData(photos);
+  });
+
+  console.log(photos);
+  setLoaded(true);
 }
