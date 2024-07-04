ee89cad5af6d0219d85696122cb51852c04c0db9
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-04 19:52:31 -0300

    Adding some text to the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,31 @@
 export default function AboutPage() {
-  return <div className="about-page page"></div>;
+  return (
+    <div className="about-page page">
+      <p>
+        Welcome to Art Gallery, where creativity meets expression! We believe in
+        the power of art to transcend boundaries and connect people from all
+        walks of life. Our platform is a digital sanctuary designed to showcase
+        your artistic journey and provide a space for exploration and
+        inspiration.
+      </p>
+      <p>
+        At Art Gallery, we celebrate diversity in creativity. Whether you're an
+        emerging artist looking to share your work with the world or an art
+        enthusiast seeking new perspectives, you'll find a home here. Our
+        gallery is a vibrant tapestry of images and ideas, where each piece
+        tells a unique story waiting to be discovered.
+      </p>
+      <p>
+        Upload your artworks with ease and explore a curated collection of
+        images from talented creators around the globe. Our intuitive tagging
+        system allows you to search for specific themes, styles, or subjects,
+        making it effortless to find pieces that resonate with you.
+      </p>
+      <p>
+        Join us on this artistic adventure. Let's build a community where
+        imagination thrives and art finds its rightful place in the spotlight.
+        Together, let's paint the digital canvas with creativity and passion.
+      </p>
+    </div>
+  );
 }
