7847c0e9dce77f235113a237086457e1d569f8ab
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-04 15:34:48 -0300

    Adding the route for /add

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 
 import MainLayout from "./Layouts/MainLayout";
 import MainPage from "./pages/MainPage";
+import AddPhotoPage from "./pages/AddPhotoPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<MainPage />}></Route>
+        <Route path="/add" element={<AddPhotoPage />}></Route>
       </Route>
     )
   );
