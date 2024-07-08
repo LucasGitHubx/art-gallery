10b2cc4766e617f24ca2c68b1d99b75f96420d73
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-08 02:48:09 -0300

    I created two new functions: 1) addFavourite 2) getFavourites
    The first one add the photo that we selected on the favourite-photos collection.
    The second one gets all the photos from the favourite-photos collection.

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -15,6 +15,7 @@ import Toastify from "toastify-js";
 
 const db = getFirestore(app);
 const photoRef = collection(db, "photos");
+const favouritePhotosRef = collection(db, "favourite-photos");
 
 export async function getAllPhotos(setData, setLoaded) {
   const photos = [];
@@ -47,25 +48,40 @@ export function deletePhoto(id, setData, tag) {
 }
 
 export async function addPhoto({ title, author, url, tags }) {
-  await addDoc(photoRef, {
-    title,
-    author,
-    tags,
-    url,
-  });
+  try {
+    await addDoc(photoRef, {
+      title,
+      author,
+      tags,
+      url,
+    });
 
-  Toastify({
-    text: "Photo added correctly",
-    duration: 3000,
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #00b09b, #3dc954)",
-    },
-  }).showToast();
+    Toastify({
+      text: "Photo added correctly",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "bottom", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #3dc954)",
+      },
+    }).showToast();
+  } catch (error) {
+    Toastify({
+      text: error.message,
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "bottom", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, red, salmon, tomato)",
+      },
+    }).showToast();
+  }
 }
 
 export async function searchByTag(setData, tag) {
@@ -84,4 +100,46 @@ export async function searchByTag(setData, tag) {
   });
 
   setData(photos);
+}
+
+export async function addFavourite({ author, title, url, tags }, person) {
+  await addDoc(favouritePhotosRef, {
+    author,
+    title,
+    url,
+    tags: tags[0],
+    person,
+  });
+
+  Toastify({
+    text: "Photo added as favourite!",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #3dc954)",
+    },
+  }).showToast();
+}
+
+export async function getFavourites(setData, setLoaded) {
+  const favouritePhotos = [];
+  const q = query(favouritePhotosRef);
+  const data = await getDocs(q);
+
+  data.docs.forEach((doc) => {
+    photos.push({
+      id: doc.id,
+      title: doc.data().title,
+      author: doc.data().author,
+      url: doc.data().url,
+      tags: Array(doc.data().tags),
+    });
+  });
+
+  setData(favouritePhotos);
+  setLoaded(true);
 }
